4571073c317cd0c0cf89fe474ddabfbf0e2ca3da
waleed <waleed.ayoub@gmail.com>
2022-11-12 19:22:32 -0500

    changed dirs

--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,7 @@
 docker run --name=valheim -d \
     --restart always \
     -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp \
-    -v /Users/temp/games/valheim/valheimdata:/home/steam/steamcmd/valheimserver/valheimdata \
+    -v /Users/temp/games/valheim/valheimdata:/home/steam/valheimserver/valheimdata \
     --env VALHEIM_SERVER_NAME="EADM" \
     --env VALHEIM_WORLD_NAME="BLIMBLAM" \
     --env VALHEIM_PASSWORD="eadm2022" \
